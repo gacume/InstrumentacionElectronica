0bc1bf003a076ad55f24ff1c02faf11eb902b6e5
basherMX <brayanvazq1608@gmail.com>
2023-06-13 17:03:52 -0600

    final

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import "./App.css";
 import axios from 'axios';
 import L from "leaflet";
 
+import carIcon from "./Vector.svg";
+
 export const icon = new L.Icon({
   iconUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/marker-icon.png",
   iconRetinaUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/marker-icon-2x.png",
@@ -37,37 +39,30 @@ export default function App() {
     };
   }, []);
 
-  const deleteAll = () => {
-    axios.patch('https://locationsbackend-production.up.railway.app/api/deleteAll')
-      .then(() => {
-        setPosts([]);
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  };
-
   useEffect(() => {
     if (posts.length > 0 && mapRef.current) {
-      const latitudes = posts.map(post => post.latitud);
-      const longitudes = posts.map(post => post.longitud);
-      const bounds = L.latLngBounds(
-        L.latLng(Math.min(...latitudes), Math.min(...longitudes)),
-        L.latLng(Math.max(...latitudes), Math.max(...longitudes))
-      );
-      mapRef.current.leafletElement.fitBounds(bounds);
+      const lastPost = posts[posts.length - 1];
+      mapRef.current.leafletElement.setView([lastPost.latitud, lastPost.longitud], 16);
     }
   }, [posts, mapRef]);
 
+  const carMarkerIcon = new L.Icon({
+    iconUrl: carIcon,
+    shadowUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/marker-shadow.png",
+    iconSize: [30,30],
+    iconAnchor: [15,30],
+    popupAnchor: [0,0],
+    tooltipAnchor: [0,0],
+    shadowSize: [41, 41]
+  });
+
   return (
     <div>
-      <Map center={[21.8853, -102.2916]} zoom={12} ref={mapRef}>
+      <Map center={[21.8853, -102.2916]} zoom={12} ref={mapRef} minZoom={0} maxZoom={18} zoomSnap={0.5}>
         <TileLayer
           url="https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}{r}.{ext}"
           attribution='Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>.'
           subdomains="abcd"
-          minZoom={0}
-          maxZoom={20}
           ext="png"
         />
 
@@ -88,12 +83,11 @@ export default function App() {
 
             <Marker
               position={[posts[posts.length - 1].latitud, posts[posts.length - 1].longitud]}
-              icon={icon}
+              icon={carMarkerIcon}
             />
           </>
         )}
       </Map>
-      <button onClick={deleteAll}>Delete All</button>
     </div>
   );
 }
