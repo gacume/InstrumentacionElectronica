599a1d441eca5d2acbb7ebc48d2a04da82d9fb23
basherMX <brayanvazq1608@gmail.com>
2023-06-13 16:19:50 -0600

    ultima version funcional

--- src/App.js
+++ src/App.js
@@ -1,60 +1,99 @@
-import React from "react";
-import { Map, Marker, Popup, TileLayer } from "react-leaflet";
-import { Icon } from "leaflet";
+import React, { useEffect, useRef } from "react";
+import { Map, Marker, Polyline, TileLayer } from "react-leaflet";
 import "./App.css";
 import axios from 'axios';
+import L from "leaflet";
 
-export const icon = new Icon({
-  iconUrl: "/skateboarding.svg",
-  iconSize: [25, 25]
+export const icon = new L.Icon({
+  iconUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/marker-icon.png",
+  iconRetinaUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/marker-icon-2x.png",
+  shadowUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/marker-shadow.png",
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  tooltipAnchor: [16, -28],
+  shadowSize: [41, 41]
 });
 
 export default function App() {
-  const [activePark, setActivePark] = React.useState(null);
   const [posts, setPosts] = React.useState([]);
-  React.useEffect(() => {
+  const mapRef = useRef(null);
+
+  const getAllData = () => {
     axios.get('https://locationsbackend-production.up.railway.app/api/getAll').then(response => {
       setPosts(response.data);
-    })
+    });
+  };
+
+  useEffect(() => {
+    getAllData();
+
+    const interval = setInterval(() => {
+      getAllData();
+    }, 5 * 60);
+
+    return () => {
+      clearInterval(interval);
+    };
   }, []);
-  console.log(posts, 'this is app')
+
+  const deleteAll = () => {
+    axios.patch('https://locationsbackend-production.up.railway.app/api/deleteAll')
+      .then(() => {
+        setPosts([]);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    if (posts.length > 0 && mapRef.current) {
+      const latitudes = posts.map(post => post.latitud);
+      const longitudes = posts.map(post => post.longitud);
+      const bounds = L.latLngBounds(
+        L.latLng(Math.min(...latitudes), Math.min(...longitudes)),
+        L.latLng(Math.max(...latitudes), Math.max(...longitudes))
+      );
+      mapRef.current.leafletElement.fitBounds(bounds);
+    }
+  }, [posts, mapRef]);
+
   return (
     <div>
-      <Map center={[21.8853, -102.2916]} zoom={12}>
+      <Map center={[21.8853, -102.2916]} zoom={12} ref={mapRef}>
         <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}{r}.{ext}"
+          attribution='Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>.'
+          subdomains="abcd"
+          minZoom={0}
+          maxZoom={20}
+          ext="png"
         />
-        {posts.map(post => (
-          <Marker
-            position={[
-              post.latitud,
-              post.longitud
-            ]}
-            onClick={() => {
-              setActivePark(post);
-            }}
-            icon={icon}
-          />
-        ))}
 
-        {activePark && (
-          <Popup
-            position={[
-              activePark.geometry.coordinates[1],
-              activePark.geometry.coordinates[0]
-            ]}
-            onClose={() => {
-              setActivePark(null);
-            }}
-          >
-            <div>
-              <h2>{activePark.properties.NAME}</h2>
-              <p>{activePark.properties.DESCRIPTIO}</p>
-            </div>
-          </Popup>
+        {posts.length > 1 && (
+          <Polyline
+            positions={posts.map(post => [post.latitud, post.longitud])}
+            color="red"
+            weight={5}
+          />
+        )}
+
+        {posts.length > 0 && (
+          <>
+            <Marker
+              position={[posts[0].latitud, posts[0].longitud]}
+              icon={icon}
+            />
+
+            <Marker
+              position={[posts[posts.length - 1].latitud, posts[posts.length - 1].longitud]}
+              icon={icon}
+            />
+          </>
         )}
       </Map>
+      <button onClick={deleteAll}>Delete All</button>
     </div>
   );
 }
--- src/Data.js
+++ src/Data.js
@@ -11,40 +11,63 @@ import axios from 'axios';
 export default function Data() {
   const [posts, setPosts] = React.useState([]);
   let deletedAll = false;
-  React.useEffect(() => {
+
+  const getAllData = () => {
     axios.get('https://locationsbackend-production.up.railway.app/api/getAll').then(response => {
       setPosts(response.data);
-    })
-  }, [deletedAll]);
+    });
+  };
 
   const deleteAll = () => {
-    axios.patch('https://locationsbackend-production.up.railway.app/api/deleteAll')
+    const confirmed = window.confirm('Are you sure you want to delete all?');
+    if (confirmed) {
+      axios.patch('https://locationsbackend-production.up.railway.app/api/deleteAll')
+        .then(() => {
+          getAllData(); // Actualizar la lista después de eliminar todos los datos
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }
   };
-  return (
 
+  React.useEffect(() => {
+    getAllData(); // Obtener los datos iniciales al cargar el componente
+
+    // Llamar a getAllData() automáticamente cada 5 minutos
+    const interval = setInterval(() => {
+      getAllData();
+    }, 5 * 60);
+
+    return () => {
+      clearInterval(interval); // Limpiar el intervalo cuando el componente se desmonte
+    };
+  }, []);
+
+  return (
     <Box sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
       <Box sx={{ paddingBottom: 8, paddingTop: 2, alignItems: 'center' }}>
-        <Typography variant='h1'>Locations</Typography>
+        <Typography variant='h4' fontWeight='bold'>Locations</Typography>
       </Box>
-      <Box>
+      <Box sx={{ maxHeight: '40vh', overflowY: 'auto' }}>
         <Divider />
         <nav aria-label="main mailbox folders">
-          {
-            posts.map(location =>
-              <List>
-                <ListItem disablePadding>
-                  <ListItemButton>
-                    <ListItemText primary={location.id} />
-                  </ListItemButton>
-                </ListItem>
-              </List>)}
+          <List>
+            {posts.map(location => (
+              <ListItem key={location.id} disablePadding>
+                <ListItemButton>
+                  <ListItemText primary={"Id: " + location.id} />
+                  <ListItemText primary={"lat: " + location.latitud} />
+                  <ListItemText primary={"long: " + location.longitud} />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
         </nav>
-        <Divider />
-        <Box sx={{ paddingTop: 2 }} onClick={() => {
-          deleteAll()
-        }}>
-          <Button variant="contained">DELETE ALL</Button>
-        </Box>
+      </Box>
+      <Divider />
+      <Box sx={{ paddingTop: 2 }} onClick={deleteAll}>
+        <Button variant="contained">DELETE ALL</Button>
       </Box>
     </Box>
   );
